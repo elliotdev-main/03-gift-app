1f2b561bf2fa19eca55178524cf25043f40de1b2
elliotdev-main <elliotdev91@gmail.com>
2026-06-06 12:01:55 +0200

    Añadiendo datos de environments

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+// import { environment } from '../../../../../environments/environment.development';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './gifs-side-menu-header.html',
+  standalone: true,
+})
+export class GifsSideMenuHeader {
+
+  envs = environment
+
+}
